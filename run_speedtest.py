1416cb527122273bc81a4719e9b38eb062c946cc
T0b145 <tobias-braun@gmx.net>
2023-04-15 07:08:35 +0000

    new chart

--- run_speedtest.py
+++ run_speedtest.py
@@ -1,8 +1,12 @@
 import csv
 import speedtest
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
 # CSV file path
 csv_file = 'data.csv'
+chart_file = 'chart.png'
 
 servers = []
 # If you want to test against a specific server
@@ -26,3 +30,26 @@ with open(csv_file, mode='a', newline='') as file:
     if file.tell() == 0:
         writer.writeheader()
     writer.writerow(results)
+
+
+
+
+# read csv file into a pandas DataFrame
+df = pd.read_csv(csv_file, delimiter=';')
+
+# convert timestamp column to datetime object
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+# compute rolling average of the last 3 measurements for download and upload speed
+df['download_ma'] = df['download'].rolling(window=3).mean()
+df['upload_ma'] = df['upload'].rolling(window=3).mean()
+
+# create a line chart for download and upload speed with moving average
+fig, ax = plt.subplots(figsize=(10, 6))
+df.plot(x='timestamp', y=['download', 'upload'], alpha=0.5, ax=ax)
+df.plot(x='timestamp', y=['download_ma', 'upload_ma'], ax=ax)
+ax.set(title='Internet Speed over Time', xlabel='Timestamp', ylabel='Speed (Mbps)')
+
+
+# save chart as png file
+plt.savefig(chart_file)
