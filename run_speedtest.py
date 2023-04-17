cb84969b9fde23eff7cd54bd807e77466bbe7ca8
T0b145 <tobias-braun@gmx.net>
2023-04-17 09:46:31 +0000

    Improving plot visual

--- run_speedtest.py
+++ run_speedtest.py
@@ -3,6 +3,41 @@ import speedtest
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+import pytz
+
+def plot_graph(csv_file, chart_file, n_days=30):
+    # read csv file into a pandas DataFrame
+    df = pd.read_csv(csv_file, delimiter=';')
+
+    # convert timestamp column to datetime object and set timezone to Germany
+    germany_tz = pytz.timezone('Europe/Berlin')
+    df['timestamp'] = pd.to_datetime(df['timestamp']).dt.tz_convert(germany_tz)
+
+    # compute rolling average of the last 3 measurements for download and upload speed
+    df['download_ma'] = df['download'].rolling(window=3).mean()
+    df['upload_ma'] = df['upload'].rolling(window=3).mean()
+
+    # filter data to last 30 days
+    last_30_days = datetime.now() - timedelta(days=30)
+    last_30_days = pd.Timestamp(last_30_days, tz=germany_tz)
+    df = df[df['timestamp'] > last_30_days]
+
+    # create a line chart for download and upload speed with moving average
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # plot upload and download as dots
+    ax.scatter(df['timestamp'], df['upload'], color='blue', alpha=0.4)
+    ax.scatter(df['timestamp'], df['download'], color='red', alpha=0.4)
+
+    # plot moving averages as lines
+    df.plot(x='timestamp', y='download_ma', color='red', ax=ax)
+    df.plot(x='timestamp', y='upload_ma', color='blue', ax=ax)
+
+    ax.set(title='Internet Speed over Time', xlabel='Timestamp', ylabel='Speed (Mbps)')
+
+    # save chart as png file
+    plt.savefig(chart_file)
 
 # CSV file path
 csv_file = 'data.csv'
@@ -31,25 +66,4 @@ with open(csv_file, mode='a', newline='') as file:
         writer.writeheader()
     writer.writerow(results)
 
-
-
-
-# read csv file into a pandas DataFrame
-df = pd.read_csv(csv_file, delimiter=';')
-
-# convert timestamp column to datetime object
-df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-# compute rolling average of the last 3 measurements for download and upload speed
-df['download_ma'] = df['download'].rolling(window=3).mean()
-df['upload_ma'] = df['upload'].rolling(window=3).mean()
-
-# create a line chart for download and upload speed with moving average
-fig, ax = plt.subplots(figsize=(10, 6))
-df.plot(x='timestamp', y=['download', 'upload'], alpha=0.5, ax=ax)
-df.plot(x='timestamp', y=['download_ma', 'upload_ma'], ax=ax)
-ax.set(title='Internet Speed over Time', xlabel='Timestamp', ylabel='Speed (Mbps)')
-
-
-# save chart as png file
-plt.savefig(chart_file)
+plot_graph(csv_file, chart_file)
